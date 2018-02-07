d7d7b7495c5438abe95b6c12975734f521187a33
lilianatang <Quyen@DESKTOP-FL7J8AP.localdomain>
2018-02-06 19:09:25 -0700

    add 2 java files to pull out the courses

---
+++ java/CourseActivity.java
@@ -0,0 +1,102 @@
+ckage ca.macewan.getgo.getgo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    //this is the JSON Data URL
+    //make sure you are using the correct ip else it will not work
+    private static final String URL_COURSES = "http://localhost:1101/backend_Getgo/get_all_courses.php";
+
+    //a list to store all the products
+    List<Course> CourseList;
+
+    //the recyclerview
+    RecyclerView recyclerView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //getting the recyclerview from xml
+        recyclerView = findViewById(R.id.recylcerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //initializing the productlist
+        CourseList = new ArrayList<>();
+
+        //this method will fetch and parse json
+        //to display it in recyclerview
+        loadCourses();
+    }
+
+    private void loadCourses() {
+
+        /*
+        * Creating a String Request
+        * The request type is GET defined by first parameter
+        * The URL is defined in the second parameter
+        * Then we have a Response Listener and a Error Listener
+        * In response listener we will get the JSON response as a String
+        * */
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL_PRODUCTS,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            //converting the string to json array object
+                            JSONArray array = new JSONArray(response);
+
+                            //traversing through all the object
+                            for (int i = 0; i < array.length(); i++) {
+
+                                //getting product object from json array
+                                JSONObject course = array.getJSONObject(i);
+
+                                //adding the product to product list
+                                CourseList.add(new Course(
+                                        course.getInt("course_id"),
+                                        course.getString("course_name"),
+                                        course.getInt("credits"),
+                                        course.getString("decription")
+                                ));
+                            }
+
+                            //creating adapter object and setting it to recyclerview
+                            CoursesAdapter adapter = new CoursesAdapter(MainActivity.this, courseList);
+                            recyclerView.setAdapter(adapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+
+        //adding our stringrequest to queue
+        Volley.newRequestQueue(this).add(stringRequest);
+    }
+}
