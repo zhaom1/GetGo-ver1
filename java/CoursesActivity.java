d5b02cf838c3abfd1f8fc89c01cf2c5862279e51
siham18 <sihamcp@gmail.com>
2018-02-22 08:22:57 -0700

    Update CoursesActivity.java

--- java/CoursesActivity.java
+++ java/CoursesActivity.java
@@ -1,8 +1,8 @@
 package com.example.getgo.getgo_v1;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import android.util.Log;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.view.View;
@@ -11,8 +11,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 /**
  * Created by Siham on 2018-02-20.
@@ -23,9 +21,6 @@ import java.util.concurrent.ExecutionException;
 
 public class CoursesActivity extends AppCompatActivity implements OnClickListener {
 
-    //a list to store all the products
-    List<Course> CourseList;
-
     /*These values are being referenced from the xml files*/
     private Button btnAdd;
     private ListView lv;
@@ -33,23 +28,21 @@ public class CoursesActivity extends AppCompatActivity implements OnClickListene
     private EditText mark_box;
     ArrayList<String> list = new ArrayList<>();
     ArrayAdapter<String> adapter;
-    ArrayList<String> Classes = new ArrayList<>();
-    ArrayAdapter<String> adapter_classes;
+    public ArrayAdapter<String> adapter_classes;
+    ArrayList<String> Classes;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        Singleton singleton = new Singleton();
-        singleton.execute();
-        JsonObject jsonObject = singleton.doInBackground();
-
-        //initializing the productlist
+        //initializing the product list
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courses);
         AutoCompleteTextView autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
 
-        //Fills the dropdown menu with the variables in the array Classes
-        adapter_classes = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, Classes);
+        //Calls Singleton to fill dropdown menu
+        adapter_classes = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item);
+        new Singleton(adapter_classes).execute();
+
         autoCompleteTextView.setAdapter(adapter_classes);
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, list);
 
@@ -61,16 +54,12 @@ public class CoursesActivity extends AppCompatActivity implements OnClickListene
 
         lv = findViewById(R.id.listView);
         lv.setAdapter(adapter);
-
-        CourseList = new ArrayList<>();
     }
 
     //When buttton is clicked, joins the strings and places in tex box
     public void onClick(View v) {
         String course_name = course_box.getText().toString();
         String course_mark = mark_box.getText().toString();
-
-        Log.d("size","Size: " + CourseList.size());
 
         if (course_mark.length() > 0 && course_mark.length() > 0) {
             String join = course_name + "    " + course_mark + "%";
